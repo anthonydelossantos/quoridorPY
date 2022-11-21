d6ee28a5d1c3e0cddf188a2b4906b3d85487b655
Tony <anthonytony2000.04@gmail.com>
2022-11-20 22:15:23 -0500

    adding Wall options with if else statement - I got some errors to solve

--- Quoridor.java
+++ Quoridor.java
@@ -10,6 +10,7 @@ public class Quoridor
           String input = new String();
           String currentplayer_history;
           boolean isGameOver = false;
+          int[] wall_position=new int[4];
           Player[] players = new Player[2];
           ArrayList <String> history_p1 = new ArrayList<String>();
           ArrayList <String> history_p2 = new ArrayList<String>();
@@ -22,7 +23,7 @@ public class Quoridor
           players[1] = p2;
           int count = 0;
           boolean moved=true;
-          
+          String accion;
           Player currentPlayer = new Player();
           
           Board Qboard = new Board(p1, p2);        
@@ -33,50 +34,68 @@ public class Quoridor
           while(!isGameOver)
                 { 
                     try {
+                         accion = "";
                          System.out.println(Qboard);
                          currentPlayer = players[count]; // tengo que hacer que cambie cada turno
                          System.out.println("Turno de " + currentPlayer +"\n");
-                         System.out.println("ENTER:|UP|+|DOWN|+|LEFT|+|RIGHT|");
-                         input = in.nextLine();
-                         //informacion del jugador 
-                         currentplayer_history = currentPlayer.name+" x:"+currentPlayer.getX()+" y:"+currentPlayer.getY();
-                         //para salir del juego
-                         if(input.equals("quit") || input.equals("QUIT") ){
-                              
-                              //optenemos historial de movimientos de cada jugador
-                              System.out.println("Historial de movimientos");
-                              for (int i = 0; i < history_p1.size(); i++) {
-                                   System.out.println(history_p1.get(i));
-
-                                   
-                              }
-                              System.out.println("-----------------------------------------------------");
-                              
-                              for (int i = 0; i < history_p2.size(); i++) {
-                                   System.out.println(history_p2.get(i));
-
-                                   
-                              }
-                              break;
+                         System.out.println("Que deseas mover?\nPeon: P || Muro: M");
+                         accion = in.nextLine();
+                         if (accion.equalsIgnoreCase("M")) {
+                              System.out.println("ENTER: (x1,y1)(x2,y2)");
                              
+                              for (int j = 0; j < 4; j++) {
+                                   System.out.print("->");
+                                   wall_position[j] = in.nextInt();
+                    
+                                   
+                              }
+                              moved = true;
                          }
-                         //movemos
-                         moved = currentPlayer.movePiece(input);
-                        
-                         while (!moved ) {
-                              System.out.println("Error.");
+                         else if(accion.equalsIgnoreCase("P")){
                               System.out.println("ENTER:|UP|+|DOWN|+|LEFT|+|RIGHT|");
                               input = in.nextLine();
-                              moved = currentPlayer.movePiece(input);
                               
-                         }
+                              //para salir del juego
+                              if(input.equalsIgnoreCase("QUIT") || input.equalsIgnoreCase("Q")){
+                                   
+                                   //optenemos historial de movimientos de cada jugador
+                                   System.out.println("Historial de movimientos");
+                                   for (int i = 0; i < history_p1.size(); i++) {
+                                        System.out.println(history_p1.get(i));
+
+                                        
+                                   }
+                                   System.out.println("-----------------------------------------------------");
+                                   
+                                   for (int i = 0; i < history_p2.size(); i++) {
+                                        System.out.println(history_p2.get(i));
+
+                                        
+                                   }
+                                   break;
+                              
+                              }
+                              //movemos
+                              moved = currentPlayer.movePiece(input);
+                         
+                              while (!moved ) {
+                                   System.out.println("Error.");
+                                   System.out.println("ENTER:|UP|+|DOWN|+|LEFT|+|RIGHT|");
+                                   input = in.nextLine();
+                                   moved = currentPlayer.movePiece(input);
+                                   
+                              }
                          
 
                          
                          
                          
                          Qboard.reDraw(p1, p2);
-                         
+            
+                    
+                         }
+                         //informacion del jugador 
+                         currentplayer_history = currentPlayer.name+" x:"+currentPlayer.getX()+" y:"+currentPlayer.getY();
                          //si el identificador de mi jugador actual es 1 le sumo uno mas 
                          //para que sea turno de dos , pero antes guardo su posicion actual en el array 
                          if (players[count].n == 1) {
@@ -90,9 +109,6 @@ public class Quoridor
                               history_p1.add(currentplayer_history);
                          }
                          
-                         
-                         
-                    
                     
                     } catch (Exception e) {
                          in.nextLine();
---
+++ README.txt
@@ -0,0 +1,2 @@
+TODO:
+Resolver error a la hora de elejir M. Luego de 	que el jugador 1 introduzca sus coordenadas se salta al jugador 2 y luego pasa al jugador 1 otra vez 
